d0a0eb27406f07acbd25a4858e6ec17fce35c659
Gifftybabe <ulokangozi@gmail.com>
2024-09-07 22:12:58 +0100

    Add allowed ports

--- django_project/settings.py
+++ django_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-*8a1sqyg@uqn83tj!=t$8$x3r1$#r(&25((g85au_5$y5d-4wh
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["0.0.0.0", "127.0.0.1"]
 
 
 # Application definition
